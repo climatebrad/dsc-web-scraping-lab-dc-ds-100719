4294ca35f0f7336849595ac72063e5bbeab9f806
Brad Johnson <climatebrad@gmail.com>
2019-10-29 17:15:36 -0400

    building parser

--- parsebooks.py
+++ parsebooks.py
@@ -1,9 +1,9 @@
 """Parse books.toscrape"""
 
 import re
+import requests
 import bs4
 from bs4 import BeautifulSoup
-import requests
 from word2number import w2n
 
 class SoupBook(bs4.element.Tag):
@@ -30,9 +30,9 @@ class SoupBook(bs4.element.Tag):
         return w2n.word_to_num(self.find('p', {"class" : regex}).attrs['class'][-1])
 
     @property
-    def price(book):
+    def price(self):
         """Returns price as float. Found in <p class='price_color'>"""
-        return re.sub(r'[^\d.]', '', self.find(class_="price_color").text)
+        return float(re.sub(r'[^\d.]', '', self.find(class_="price_color").text))
 
     @property
     def availability(self):
@@ -52,12 +52,31 @@ def retrieve_books(soup):
     """Return a list of SoupBooks.
     Found in <article class="product_pod">"""
     books = soup.select(".product_pod")
-    map (SoupBook.convert_to_soupbook, books)
+    for book in books:
+        SoupBook.convert_to_soupbook(book)
     return books
 
 def retrieve_book_dicts(soup):
     """Returns list of dicts of books, with title, rating, price, and availability."""
     books = []
-    for book in retrieve_book_containers(soup):
+    for book in retrieve_books(soup):
         books.append(book.as_dict())
     return books
+
+def next_page(base_url, soup):
+    """Returns the url of the next page, if it exists.
+Found in <li class="next"><a href="...">"""
+    next_page = soup.find('li', class_='next')
+    if next_page:
+        return requests.compat.urljoin(base_url, next_page.a['href'])
+
+def traverse_book_soup(base_url, soup = None):
+    """Returns the soup of the base_url if soup not specified.
+Otherwise, returns the soup of the next page, if exists.
+Otherwise returns None."""
+    if not soup:
+        next_url = base_url
+    else:
+        next_url = next_page(base_url, soup)
+    if next_url:
+        return BeautifulSoup(requests.get(next_url).content, 'html.parser')
